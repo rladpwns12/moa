5b21b40df224abd9126d72073c3e4cea5269875d
kjh9478 <jiho9478!@gmail.com>
2019-08-05 20:00:56 +0900

    1.임시 제출용2

---
+++ src/main/java/com/moa/model/dao/HostReputationDAO.java
@@ -0,0 +1,7 @@
+package com.moa.model.dao;
+
+import com.example.springmvc.vo.HostReputationVO;
+
+public interface HostReputationDAO {
+    HostReputationVO searchOne(int articleNum);
+}
---
+++ src/main/java/com/moa/model/service/StoreBoardService.java
@@ -0,0 +1,7 @@
+package com.moa.model.service;
+
+import java.util.Map;
+
+public interface StoreBoardService {
+    public Map<String,Object> selectStorage(int articleNum);
+}
