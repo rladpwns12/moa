e5567d3166aa7ec98e30de070ca93bfa79fc076a
kjh9478 <jiho9478!@gmail.com>
2019-08-06 22:17:23 +0900

    1.임시 제출용_v0.7

---
+++ src/main/java/com/moa/controller/KeepController.java
@@ -0,0 +1,16 @@
+/*
+package com.moa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/storeBoard")
+@Controller
+public class KeepController {
+	@RequestMapping("/keep")
+	public String registerStoreBoard() {
+		return "keep";
+	}
+}
+*/
